61bb4032d25b0d47fd46f1ab51ba38495dc6d1a4
Esger <esgerj@gmail.com>
2023-03-13 21:58:14 +0100

    Pulsors

--- src/resources/elements/controls/controls.js
+++ src/resources/elements/controls/controls.js
@@ -11,17 +11,23 @@ export class ControlsCustomElement {
 	constructor(eventAggregator) {
 		this._eventAggregator = eventAggregator;
 		this.clearPulsor = false;
+		this.stopPulsor = false;
+		this.startPulsor = true;
+		this.randomPulsor = false;
 		this.timeOut = 0;
-		this.addListeners();
 	}
 
 	clear() {
 		this._eventAggregator.publish('clear');
 		this.clearPulsor = false;
+		this.stopPulsor = false;
+		this.randomPulsor = true;
 	}
 
 	stop() {
 		this._eventAggregator.publish('stop');
+		this.stopPulsor = false;
+		this.clearPulsor = true;
 	}
 
 	step() {
@@ -30,20 +36,17 @@ export class ControlsCustomElement {
 
 	start() {
 		this._eventAggregator.publish('start');
+		this.startPulsor = false;
+		this.stopPulsor = true;
 	}
 
 	fillRandom() {
 		this._eventAggregator.publish('fillRandom');
+		this.randomPulsor = false;
+		this.startPulsor = true;
 	}
 
 	setTimeoutInterval() {
 		this._eventAggregator.publish('timeoutInterval', this.timeOut);
 	}
-
-	addListeners() {
-		this._eventAggregator.subscribe('cellSize', response => {
-			this.clearPulsor = true;
-		});
-	}
-
 }
